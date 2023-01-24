0b6c89ff8796833d5614ecba61a5c60f9c5df22f
David Cheung <dcheung2@andrew.cmu.edu>
2023-01-24 15:13:20 -0500

    Add high level plan of implementation

--- stat_display.py
+++ stat_display.py
@@ -4,3 +4,9 @@ dwg = svgwrite.Drawing('test.svg')
 dwg.add(dwg.line((0, 100), (100, 0), stroke=svgwrite.rgb(10, 10, 16, '%')))
 dwg.add(dwg.text('Test', insert=(0, 20), fill='red'))
 dwg.save()
+
+# TODO: Create base image
+# TODO: Add text for stats at each corner
+# TODO: Use lines first stemming from circle
+# TODO: Figure out how to variate the shape in the middle to display stats
+# TODO: Paramertize output name to match other files
