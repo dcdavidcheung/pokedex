bb91ae45b80a404f7479cdb341330977fdc2b3e2
David Cheung <dcheung2@andrew.cmu.edu>
2023-03-13 17:05:07 -0400

    Need to figure out how to add transparent borders

--- stat_display.py
+++ stat_display.py
@@ -5,7 +5,7 @@ import math
 IMG_SIZE = 900
 SIZE = 720
 
-dwg = svgwrite.Drawing('test.svg', size=(SIZE,SIZE))
+dwg = svgwrite.Drawing('test.svg', size=(IMG_SIZE,IMG_SIZE))
 # dwg.add(dwg.line((0, 100), (100, 0), stroke=svgwrite.rgb(10, 10, 16, '%')))
 # dwg.add(dwg.text('Test', insert=(0, 20), fill='red'))
 # dwg.add(dwg.polygon([(0,0), (10,10), (10,30)], fill='blue'))
@@ -29,47 +29,80 @@ TEST = [100, 120,  130,  90,  60, 110]
 
 # input: stats -> expects list of stats in order of HP, ATK, DEF, SPA, SPD, SPE
 # output:      -> returns point list for SVG polygon
-def gen_polygon(stats):
+def gen_polygon_stats(stats):
     ret_L = []
     # HP
     # Line from center straight up (SIZE/2,SIZE/2)->(SIZE/2,0)
-    length = TEST[0]/HIGHEST_STAT * SIZE/2
+    length = stats[0]/HIGHEST_STAT * SIZE/2
     ret_L.append((int(SIZE/2),int(SIZE/2-length)))
 
     # ATK
     # Line from center 30 degrees above right perpendicular (SIZE/2,SIZE/2)->(SIZE-SIZE*SQRT(3)/4,SIZE/4)
-    length = TEST[1]/HIGHEST_STAT * SIZE/2
+    length = stats[1]/HIGHEST_STAT * SIZE/2
     ret_L.append((int(SIZE/2+length*math.sqrt(3)/2),int(SIZE/2-length/2)))
 
     # DEF
     # Line from center 30 degrees below right perpendicular (SIZE/2,SIZE/2)->(SIZE-SIZE*SQRT(3)/4,3*SIZE/4)
-    length = TEST[2]/HIGHEST_STAT * SIZE/2
+    length = stats[2]/HIGHEST_STAT * SIZE/2
     ret_L.append((int(SIZE/2+length*math.sqrt(3)/2),int(SIZE/2+length/2)))
 
     # SPE
     # Line from center straight down (SIZE/2,SIZE/2)->(SIZE/2,SIZE)
-    length = TEST[5]/HIGHEST_STAT * SIZE/2
+    length = stats[5]/HIGHEST_STAT * SIZE/2
     ret_L.append((int(SIZE/2),int(SIZE/2+length)))
 
     # SPD
     # Line from center 30 degrees below left perpendicular (SIZE/2,SIZE/2)->(SIZE/2-SIZE*SQRT(3)/2,3*SIZE/4)
-    length = TEST[4]/HIGHEST_STAT * SIZE/2
+    length = stats[4]/HIGHEST_STAT * SIZE/2
     ret_L.append((int(SIZE/2-length*math.sqrt(3)/2),int(SIZE/2+length/2)))
 
     # SPA
     # Line from center 30 degrees above left perpendicular (SIZE/2,SIZE/2)->(SIZE/2-SIZE*SQRT(3)/4,SIZE/4)
-    length = TEST[3]/HIGHEST_STAT * SIZE/2
+    length = stats[3]/HIGHEST_STAT * SIZE/2
     ret_L.append((int(SIZE/2-length*math.sqrt(3)/2),int(SIZE/2-length/2)))
 
-    # for stat in stats:
-    #     ret_L.append()
-    print(ret_L)
+    return ret_L
+
+# Draw hexagons
+def gen_polygon(length):
+    ret_L = []
+    # HP
+    # Line from center straight up (SIZE/2,SIZE/2)->(SIZE/2,0)
+    # length = stats[0]/HIGHEST_STAT * SIZE/2
+    ret_L.append((int(SIZE/2),int(SIZE/2-length)))
+
+    # ATK
+    # Line from center 30 degrees above right perpendicular (SIZE/2,SIZE/2)->(SIZE-SIZE*SQRT(3)/4,SIZE/4)
+    # length = stats[1]/HIGHEST_STAT * SIZE/2
+    ret_L.append((int(SIZE/2+length*math.sqrt(3)/2),int(SIZE/2-length/2)))
+
+    # DEF
+    # Line from center 30 degrees below right perpendicular (SIZE/2,SIZE/2)->(SIZE-SIZE*SQRT(3)/4,3*SIZE/4)
+    # length = stats[2]/HIGHEST_STAT * SIZE/2
+    ret_L.append((int(SIZE/2+length*math.sqrt(3)/2),int(SIZE/2+length/2)))
+
+    # SPE
+    # Line from center straight down (SIZE/2,SIZE/2)->(SIZE/2,SIZE)
+    # length = stats[5]/HIGHEST_STAT * SIZE/2
+    ret_L.append((int(SIZE/2),int(SIZE/2+length)))
+
+    # SPD
+    # Line from center 30 degrees below left perpendicular (SIZE/2,SIZE/2)->(SIZE/2-SIZE*SQRT(3)/2,3*SIZE/4)
+    # length = stats[4]/HIGHEST_STAT * SIZE/2
+    ret_L.append((int(SIZE/2-length*math.sqrt(3)/2),int(SIZE/2+length/2)))
+
+    # SPA
+    # Line from center 30 degrees above left perpendicular (SIZE/2,SIZE/2)->(SIZE/2-SIZE*SQRT(3)/4,SIZE/4)
+    # length = stats[3]/HIGHEST_STAT * SIZE/2
+    ret_L.append((int(SIZE/2-length*math.sqrt(3)/2),int(SIZE/2-length/2)))
+
     return ret_L
 
 def add_polygon(points):
     dwg.add(dwg.polygon(points, fill='gray'))
 
-dwg.add(dwg.polygon([(0,0),(SIZE,0),(SIZE,SIZE),(0,SIZE)], fill='blue'))
-dwg.add(dwg.polygon(gen_polygon(TEST), fill='yellow'))
+dwg.add(dwg.polygon([(0,0),(IMG_SIZE,0),(IMG_SIZE,IMG_SIZE),(0,IMG_SIZE)], fill='blue'))
+dwg.add(dwg.polygon(gen_polygon_stats(TEST), fill='yellow'))
+dwg.add(dwg.polyline(gen_polygon(((IMG_SIZE-SIZE)/2+SIZE)/2), fill='none'))
 # add_polygon(TEST)
 dwg.save()
