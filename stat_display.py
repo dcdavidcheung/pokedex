9d5dff17f96a134794a20c98c90fb6f25cb77438
David Cheung <dcheung2@andrew.cmu.edu>
2023-03-14 13:18:41 -0400

    Renders correctly now, need to add text now

--- stat_display.py
+++ stat_display.py
@@ -24,8 +24,8 @@ dwg = svgwrite.Drawing('test.svg', size=(IMG_SIZE,IMG_SIZE))
 
 # Need to grab the highest overall stat and have that be the scaling factor
 HIGHEST_STAT = 150
-TEST = [100, 120,  130,  90,  60, 110]
-# TEST = [100, 100, 100, 100, 100, 100]
+# TEST = [100, 120,  130,  90,  60, 110]
+TEST = [100, 100, 100, 100, 100, 100]
 
 # input: stats -> expects list of stats in order of HP, ATK, DEF, SPA, SPD, SPE
 # output:      -> returns point list for SVG polygon
@@ -34,32 +34,32 @@ def gen_polygon_stats(stats):
     # HP
     # Line from center straight up (SIZE/2,SIZE/2)->(SIZE/2,0)
     length = stats[0]/HIGHEST_STAT * SIZE/2
-    ret_L.append((int(SIZE/2),int(SIZE/2-length)))
+    ret_L.append((int(IMG_SIZE/2),int(IMG_SIZE/2-length)))
 
     # ATK
     # Line from center 30 degrees above right perpendicular (SIZE/2,SIZE/2)->(SIZE-SIZE*SQRT(3)/4,SIZE/4)
     length = stats[1]/HIGHEST_STAT * SIZE/2
-    ret_L.append((int(SIZE/2+length*math.sqrt(3)/2),int(SIZE/2-length/2)))
+    ret_L.append((int(IMG_SIZE/2+length*math.sqrt(3)/2),int(IMG_SIZE/2-length/2)))
 
     # DEF
     # Line from center 30 degrees below right perpendicular (SIZE/2,SIZE/2)->(SIZE-SIZE*SQRT(3)/4,3*SIZE/4)
     length = stats[2]/HIGHEST_STAT * SIZE/2
-    ret_L.append((int(SIZE/2+length*math.sqrt(3)/2),int(SIZE/2+length/2)))
+    ret_L.append((int(IMG_SIZE/2+length*math.sqrt(3)/2),int(IMG_SIZE/2+length/2)))
 
     # SPE
     # Line from center straight down (SIZE/2,SIZE/2)->(SIZE/2,SIZE)
     length = stats[5]/HIGHEST_STAT * SIZE/2
-    ret_L.append((int(SIZE/2),int(SIZE/2+length)))
+    ret_L.append((int(IMG_SIZE/2),int(IMG_SIZE/2+length)))
 
     # SPD
     # Line from center 30 degrees below left perpendicular (SIZE/2,SIZE/2)->(SIZE/2-SIZE*SQRT(3)/2,3*SIZE/4)
     length = stats[4]/HIGHEST_STAT * SIZE/2
-    ret_L.append((int(SIZE/2-length*math.sqrt(3)/2),int(SIZE/2+length/2)))
+    ret_L.append((int(IMG_SIZE/2-length*math.sqrt(3)/2),int(IMG_SIZE/2+length/2)))
 
     # SPA
     # Line from center 30 degrees above left perpendicular (SIZE/2,SIZE/2)->(SIZE/2-SIZE*SQRT(3)/4,SIZE/4)
     length = stats[3]/HIGHEST_STAT * SIZE/2
-    ret_L.append((int(SIZE/2-length*math.sqrt(3)/2),int(SIZE/2-length/2)))
+    ret_L.append((int(IMG_SIZE/2-length*math.sqrt(3)/2),int(IMG_SIZE/2-length/2)))
 
     return ret_L
 
@@ -69,40 +69,46 @@ def gen_polygon(length):
     # HP
     # Line from center straight up (SIZE/2,SIZE/2)->(SIZE/2,0)
     # length = stats[0]/HIGHEST_STAT * SIZE/2
-    ret_L.append((int(SIZE/2),int(SIZE/2-length)))
+    ret_L.append((int(IMG_SIZE/2),int(IMG_SIZE/2-length)))
 
     # ATK
     # Line from center 30 degrees above right perpendicular (SIZE/2,SIZE/2)->(SIZE-SIZE*SQRT(3)/4,SIZE/4)
     # length = stats[1]/HIGHEST_STAT * SIZE/2
-    ret_L.append((int(SIZE/2+length*math.sqrt(3)/2),int(SIZE/2-length/2)))
+    ret_L.append((int(IMG_SIZE/2+length*math.sqrt(3)/2),int(IMG_SIZE/2-length/2)))
 
     # DEF
     # Line from center 30 degrees below right perpendicular (SIZE/2,SIZE/2)->(SIZE-SIZE*SQRT(3)/4,3*SIZE/4)
     # length = stats[2]/HIGHEST_STAT * SIZE/2
-    ret_L.append((int(SIZE/2+length*math.sqrt(3)/2),int(SIZE/2+length/2)))
+    ret_L.append((int(IMG_SIZE/2+length*math.sqrt(3)/2),int(IMG_SIZE/2+length/2)))
 
     # SPE
     # Line from center straight down (SIZE/2,SIZE/2)->(SIZE/2,SIZE)
     # length = stats[5]/HIGHEST_STAT * SIZE/2
-    ret_L.append((int(SIZE/2),int(SIZE/2+length)))
+    ret_L.append((int(IMG_SIZE/2),int(IMG_SIZE/2+length)))
 
     # SPD
     # Line from center 30 degrees below left perpendicular (SIZE/2,SIZE/2)->(SIZE/2-SIZE*SQRT(3)/2,3*SIZE/4)
     # length = stats[4]/HIGHEST_STAT * SIZE/2
-    ret_L.append((int(SIZE/2-length*math.sqrt(3)/2),int(SIZE/2+length/2)))
+    ret_L.append((int(IMG_SIZE/2-length*math.sqrt(3)/2),int(IMG_SIZE/2+length/2)))
 
     # SPA
     # Line from center 30 degrees above left perpendicular (SIZE/2,SIZE/2)->(SIZE/2-SIZE*SQRT(3)/4,SIZE/4)
     # length = stats[3]/HIGHEST_STAT * SIZE/2
-    ret_L.append((int(SIZE/2-length*math.sqrt(3)/2),int(SIZE/2-length/2)))
-
+    ret_L.append((int(IMG_SIZE/2-length*math.sqrt(3)/2),int(IMG_SIZE/2-length/2)))
+    # print(ret_L)
     return ret_L
 
 def add_polygon(points):
     dwg.add(dwg.polygon(points, fill='gray'))
 
-dwg.add(dwg.polygon([(0,0),(IMG_SIZE,0),(IMG_SIZE,IMG_SIZE),(0,IMG_SIZE)], fill='blue'))
-dwg.add(dwg.polygon(gen_polygon_stats(TEST), fill='yellow'))
-dwg.add(dwg.polyline(gen_polygon(((IMG_SIZE-SIZE)/2+SIZE)/2), fill='none'))
+dwg.add(dwg.polygon([(0,0),(IMG_SIZE,0),(IMG_SIZE,IMG_SIZE),(0,IMG_SIZE)], fill='beige'))
+dwg.add(dwg.polygon(gen_polygon_stats(TEST), fill='#64daec'))
+# Have text for stat and number here
+dwg.add(dwg.polygon(gen_polygon(((IMG_SIZE-SIZE)/2+SIZE)/2), stroke="red", fill="none"))
+dwg.add(dwg.polygon(gen_polygon(SIZE/2), stroke="green", fill="none"))
+dwg.add(dwg.polygon(gen_polygon(SIZE*3/8), stroke="green", fill="none"))
+dwg.add(dwg.polygon(gen_polygon(SIZE/4), stroke="green", fill="none"))
+dwg.add(dwg.polygon(gen_polygon(SIZE/8), stroke="green", fill="none"))
+dwg.add(dwg.polygon(gen_polygon(SIZE/32), stroke="green", fill="none"))
 # add_polygon(TEST)
 dwg.save()
